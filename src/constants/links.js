04f3e9c124f2c8c687ac17e5afe1cdcfd52efd94
Ekjot Singh <ekjotmakhija@gmail.com>
2024-10-26 23:46:40 +0530

    first commit

---
+++ src/constants/links.js
@@ -0,0 +1,13 @@
+export default {
+  webDesign: '/services/web-design/',
+  webDevelopment: '/services/web-development/',
+  caseStudies: '/case-studies/',
+  blog: '/blog/',
+  about: '/about/',
+  github: 'https://github.com/pixel-point',
+  twitter: 'https://twitter.com/alex_barashkov',
+  email: 'mailto:info@pixelpoint.io',
+  getInTouch: 'https://surveys.pixelpoint.io/work-with-us',
+  gatsbyStarter: 'https://github.com/pixel-point/gatsby-tailwind-starter',
+  nextjsStarter: 'https://github.com/pixel-point/nextjs-tailwind-starter',
+};
